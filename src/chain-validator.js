7928ca69827b276988ea7278cd27f16f12331447
Chunpeng Huo <huocp@me.com>
2017-04-11 13:33:18 +1000

    chain validator.

---
+++ src/chain-validator.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+
+export default function (validators) {
+  return scope => {
+    let results = [];
+
+    _.each(validators, validator => {
+      const result = validator.validate(scope);
+      results.push(result);
+
+      // early break
+      if (result && result.break) return false;
+    });
+
+    return results;
+  };
+}
---
+++ test/chain-validator.spec.js
@@ -0,0 +1,34 @@
+import test from 'tape';
+import _ from 'lodash';
+import chainValidator from '../src/chain-validator';
+
+const validators = [
+  {validate: (scope) => ({t:0, break: scope.breakAt0})},
+  {validate: (scope) => ({t:1, break: scope.breakAt1})},
+  {validate: (scope) => ({t:2, break: scope.breakAt2})},
+];
+
+const chain = chainValidator(validators);
+
+test('chainValidator: chains results', t => {
+  const result = chain({});
+  t.equal(result.length, 3);
+  t.deepEqual(_.map(result, 't'), [0, 1, 2]);
+  t.end();
+});
+
+test('chainValidator: support early break', t => {
+  let result = chain({breakAt0: true});
+  t.equal(result.length, 1);
+  t.deepEqual(_.map(result, 't'), [0]);
+
+  result = chain({breakAt1: true});
+  t.equal(result.length, 2);
+  t.deepEqual(_.map(result, 't'), [0, 1]);
+
+
+  result = chain({breakAt2: true});
+  t.equal(result.length, 3);
+  t.deepEqual(_.map(result, 't'), [0, 1, 2]);
+  t.end();
+});
