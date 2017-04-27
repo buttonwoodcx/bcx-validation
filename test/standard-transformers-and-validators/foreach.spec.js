a285b537a373f3660760083b514d8d7fe0a50af2
Chunpeng Huo <huocp@me.com>
2017-04-27 16:36:23 +1000

    add test coverage on stacked switch.

--- test/standard-transformers-and-validators/foreach.spec.js
+++ test/standard-transformers-and-validators/foreach.spec.js
@@ -305,3 +305,92 @@ test('foreach: validates simple array', t => {
 
   t.end();
 });
+
+test('foreach: support multiple switch, merges result, removes duplicates', t => {
+  let rule = {
+    users: {
+      foreach: [{
+        switch: 'type',
+        cases: {
+          customer: {
+            email: ["notMandatory", "email"],
+            phone: ["notMandatory", "unique"],
+            name: ["mandatory", "unique"],
+            age: ["notMandatory", {validate: 'number', min: 16}]
+          },
+        }
+      },
+      {
+        switch: 'type',
+        cases: {
+          customer: {
+            email: ["notMandatory", "email"],
+            name: ["mandatory", "unique", {validate: 'string', minLength: 2}],
+          },
+          dealer: {
+            dealerId: ["mandatory", "unique"],
+            phone: ["mandatory", "unique"],
+            email: ["mandatory", "email"],
+            name: ["mandatory", "unique"]
+          }
+        }
+      }],
+      key: "id"
+    }
+  };
+
+  const obj = {
+    users: [
+      {id: 'c01', type: 'customer', name: 'Arm', email: 'arm@test.com'},
+
+      {id: 'd01', type: 'dealer', name: 'Dealer A', email: 'arm@test.com'},
+
+      {id: 'c02', type: 'customer', name: 'Bob', email: 'bob@test.com'},
+      {id: 'c03', type: 'customer', name: 'Bob', email: 'bob', age: 15},
+      {id: 'c04', type: 'customer', name: '', age: 18},
+      {id: 'c05', type: 'customer', name: 'X', email: 'x', age: 15},
+
+      {id: 'd02', dealerId: 'dealer.b', type: 'dealer', name: 'Dealer B', email: 'on', phone: '02123'},
+      {id: 'd03', dealerId: 'dealer.b', type: 'dealer', name: 'Dealer B', email: 'b@test.com', phone: '02123'},
+      {id: 'd04', dealerId: 'dealer.d', type: 'dealer', name: 'Dealer D', email: 'd@test.com', phone: '964324'},
+    ]
+  };
+
+  t.deepEqual(v.validate(obj, rule), {
+    users: {
+      d01: {
+        phone: ['must not be empty'],
+        dealerId: ['must not be empty'],
+      },
+      c02: {
+        name: ['must be unique']
+      },
+      c03: {
+        name: ['must be unique'],
+        email: ['not a valid email'],
+        age: ['must be at least 16']
+      },
+      c04: {
+        name: ['must not be empty'],
+      },
+      c05: {
+        name: ['must have at least 2 characters'],
+        email: ['not a valid email'],
+        age: ['must be at least 16']
+      },
+      d02: {
+        dealerId: ['must be unique'],
+        name: ['must be unique'],
+        email: ['not a valid email'],
+        phone: ['must be unique']
+      },
+      d03: {
+        dealerId: ['must be unique'],
+        name: ['must be unique'],
+        phone: ['must be unique']
+      }
+    }
+  });
+
+  t.end();
+});
